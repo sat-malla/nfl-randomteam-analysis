470924197379a6af6d97fbc25be416d46fc87d77
Sathvik Malla <sathvikmalla17@berkeley.edu>
2026-08-26 23:06:30 -0700

    feat: add back firebase analytics for prod

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,25 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Slot } from "expo-router";
-// import { getAnalytics, logScreenView } from "@react-native-firebase/analytics";
-// import { usePathname } from "expo-router";
-// import { useEffect } from "react";
+import { getAnalytics, logScreenView } from "@react-native-firebase/analytics";
+import { usePathname } from "expo-router";
+import { useEffect } from "react";
 
 import "@/global.css";
 
-// function useScreenTracking() {
-//   const pathname = usePathname();
-//   const analyticsInstance = getAnalytics();
+function useScreenTracking() {
+  const pathname = usePathname();
+  const analyticsInstance = getAnalytics();
 
-//   useEffect(() => {
-//     logScreenView(analyticsInstance, {
-//       screen_name: pathname,
-//       screen_class: pathname,
-//     });
-//   }, [pathname]);
-// }
+  useEffect(() => {
+    logScreenView(analyticsInstance, {
+      screen_name: pathname,
+      screen_class: pathname,
+    });
+  }, [pathname]);
+}
 
 export default function RootLayout() {
-  // useScreenTracking();
+  useScreenTracking();
 
   return (
     <GluestackUIProvider>
